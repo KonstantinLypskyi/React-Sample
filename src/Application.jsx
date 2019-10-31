7eda2eb00023edd9f5e2ad239f925e44432889c4
Konstantin Lypskyi <konstantinlypskyi@gmail.com>
2019-10-31 14:20:49 +0200

    cherry-pick

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 
 const Application = () => {
+<<<<<<< HEAD
+=======
+    console.log(1);
+    console.log(2);
+    console.log(3);
+
+>>>>>>> 582de25... third-commit
     return (
         <div>
             Sample
